da8271e50468dcf8f89ed39928844c90c0e9ecf5
i-qiqi <1067967678@qq.com>
2019-05-29 15:28:43 +0800

    update host

--- app/js/constants.js
+++ app/js/constants.js
@@ -1,14 +1,14 @@
-const vesselA_server = "http://10.131.245.96:9001/vessel-A/";
+const vesselA_server = "http://ssp-vessel:9001/vessel-A/";
 //logistic
-const logisticA_server = "http://10.131.245.96:9031/logistics-A/";
+const logisticA_server = "http://ssp-logistics:9031/logistics-A/";
 
 //device
 const zoomInVal = 500;
 const defaultDelayHour = 5;
 
 //coordinator
-const msc_server = "http://10.131.245.96:9042/msc/";
-const vmc_server = "http://10.131.245.96:9041/vmc/";
+const msc_server = "http://ssp-msc:9042/msc/";
+const vmc_server = "http://ssp-vmc:9041/vmc/";
 
 
 // map basePath
